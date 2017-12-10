40acafdf03600b84f690a5686f207073a1710244
Debian <admin@ip-172-31-29-198.ec2.internal>
2017-12-10 20:59:24 +0000

    logs for other tets

--- scripts/stress_time.sh
+++ scripts/stress_time.sh
@@ -3,6 +3,6 @@ size="180"
 
 for timebudget in $(seq 2 10 ${size}); do
 echo ${timebudget}
-bin/stress_std ${timebudget}	| grep 'TimeBudget\|ChosenN\|TimeUsed\|Score' >>  cysg_logs/stress_time_par36_v2.log 
+bin/stress_std ${timebudget}	| grep 'TimeBudget\|ChosenN\|TimeUsed\|Score' >>  cysg_logs/stress_time_par36_othertests.log 
 
 done
