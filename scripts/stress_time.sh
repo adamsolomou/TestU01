7217c4416e842cfde953c98348d4e051cc63f8c0
Debian <admin@ip-172-31-29-198.ec2.internal>
2017-12-10 16:21:25 +0000

    comparison between 2 tasks and sequential

--- scripts/stress_time.sh
+++ scripts/stress_time.sh
@@ -3,6 +3,6 @@ size="180"
 
 for timebudget in $(seq 2 10 ${size}); do
 echo ${timebudget}
-bin/stress_std ${timebudget}	| grep 'TimeBudget\|ChosenN\|TimeUsed\|Score' >>  cysg_logs/stress_time_ref.log 
+bin/stress_std ${timebudget}	| grep 'TimeBudget\|ChosenN\|TimeUsed\|Score' >>  cysg_logs/stress_time_ref_full.log 
 
 done
