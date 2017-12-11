e5c7d24965ed508309bf7e9600dabe39c1a68e85
Debian <admin@ip-172-31-81-0.ec2.internal>
2017-12-11 23:54:50 +0000

    increase workload again

--- scripts/stress_time.sh
+++ scripts/stress_time.sh
@@ -3,6 +3,6 @@ size="180"
 
 for timebudget in $(seq 2 10 ${size}); do
 echo ${timebudget}
-bin/stress_std ${timebudget}	| grep 'TimeBudget\|ChosenN\|TimeUsed\|Score' >>  cysg_logs/stress_time_par_all_v2.log 
+bin/stress_std ${timebudget}	| grep 'TimeBudget\|ChosenN\|TimeUsed\|Score' >>  cysg_logs/stress_time_par_all_v3.log 
 
 done
