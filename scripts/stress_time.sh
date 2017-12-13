c44212bed9bd6b36f974f7fbc9dcc4df1c83570a
Debian <admin@ip-172-31-24-205.ec2.internal>
2017-12-13 20:01:39 +0000

    update stress script

--- scripts/stress_time.sh
+++ scripts/stress_time.sh
@@ -1,9 +1,9 @@
 mkdir -p cysg_logs
 size="180"
 
-for timebudget in $(seq 2 10 ${size}); do
+for timebudget in $(seq 2  ${size}); do
 echo ${timebudget}
 
-bin/stress_std ${timebudget}	| grep 'TimeBudget\|ChosenN\|TimeUsed\|Score' >>  cysg_logs/stress_time_par_all_v1.log 
+bin/stress_std ${timebudget}	| grep 'TimeBudget\|ChosenN\|TimeUsed\|Score' >>  cysg_logs/stress_time_final_v1.log 
 
 done
