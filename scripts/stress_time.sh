d816985be890751e811b82ee398ed9e464bb81ae
Debian <admin@ip-172-31-26-19.ec2.internal>
2017-12-14 11:26:23 +0000

    intermed result

--- scripts/stress_time.sh
+++ scripts/stress_time.sh
@@ -1,9 +1,9 @@
 mkdir -p cysg_logs
 size="180"
 
-for timebudget in $(seq 2 ${size}); do
+for timebudget in $(seq 81 ${size}); do
 echo ${timebudget}
 
-bin/stress_std ${timebudget}	| grep 'TimeBudget\|ChosenN\|TimeUsed\|Score' >>  cysg_logs/stress_time_final_v2.log 
+bin/stress_std ${timebudget}	| grep 'TimeBudget\|ChosenN\|TimeUsed\|Score' >>  cysg_logs/stress_time_final_last.log 
 
 done
